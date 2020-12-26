a11ba399c8f469727775a08c1bdfee916eb4c383
Nolan Eckert <nolan.eckert@gmail.com>
2020-12-26 16:55:38 -0700

    sayHello function examples

---
+++ hello.js
@@ -0,0 +1,16 @@
+// //code example one
+// const sayHello = function () {
+//   console.log ("Hello, world");
+// }
+
+// sayHello();
+
+
+//code example two
+const sayHello = function (name) {
+  console.log ("Hello, " + name);
+}
+
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
