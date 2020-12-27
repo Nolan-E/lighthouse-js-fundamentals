0631711765ed5d173aedf96ab8f70ffeb9983d91
Nolan Eckert <nolan.eckert@gmail.com>
2020-12-26 17:06:58 -0700

    updated hello.js and added even.js

---
+++ even.js
@@ -0,0 +1,10 @@
+const isEven = function(num) {
+  return num % 2 === 0;
+}
+
+// assigning return statement to variable unnecessary, unless we want to recall this value at a later time.
+// const tenIsEven = isEven(10);
+// const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
--- hello.js
+++ hello.js
@@ -6,11 +6,25 @@
 // sayHello();
 
 
-//code example two
-const sayHello = function (name) {
-  console.log ("Hello, " + name);
-}
+// //code example two
+// const sayHello = function (name) {
+//   console.log ("Hello, " + name);
+// }
 
-sayHello("Caliban");
-sayHello("Miranda");
-sayHello("Ferdinand");
+// sayHello("Caliban");
+// sayHello("Miranda");
+// sayHello("Ferdinand");
+
+
+//code example three
+const sayHelloToConsole = function (name) {
+  console.log("Hello, "+name);
+}
+sayHelloToConsole('John');
+
+//code example four
+const returnSayHello = function (name) {
+  return "Hello, "+name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
