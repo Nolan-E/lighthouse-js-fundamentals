b36e79a1b4e514eaef6a9c688f7952593ef80f7c
Nolan Eckert <nolan.eckert@gmail.com>
2020-12-26 22:03:08 -0700

    Added voting-station-calculation.js - Voting station challenge

---
+++ voting-station-calculation.js
@@ -0,0 +1,21 @@
+const allStations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre'],
+  ['Codeville Higher Learning', 15 'school'],
+  ['Farro Pizza', 50, 'restaurant'],
+  ['Legacy Lake Hall', 150, 'community centre']
+  ]
+];
+
+const chooseStations = function (stations) {
+  let goodStations = [];
+  for (let i = 0; i < stations.length; i++) {
+    if ((stations[i][1] >= 20) && (stations[i][2] === 'community centre' || stations[i][2] === 'school')) {
+      goodStations.push(stations[i][0]);
+    }
+  }
+  return goodStations;
+};
+
+console.log(chooseStations(allStations));
