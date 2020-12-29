9914f81a9e004c66e673bd387221be5e103501dc
Nolan Eckert <nolan.eckert@gmail.com>
2020-12-28 23:20:41 -0700

    Added looping-range.js - Looping - Range assignment.

---
+++ looping-range.js
@@ -0,0 +1,17 @@
+function range(start, end, step) {
+  let output = [];
+  if (start === undefined || end === undefined || step === undefined || start > end || step <= 0) {
+    output = [];
+    console.log(start, end, step);
+  } else {
+    for (let i = start; i <= end; i += step) {
+      output.push(i);
+    }
+  }
+  return output;
+}
+
+
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
